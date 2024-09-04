aba7a30cb26f4973eee3f9c3fd176fb172b4f1aa
Geoffrey Hunter <gbmhunter@gmail.com>
2024-09-04 21:22:27 +1200

    Vector example is now working.

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ mkdir -p build/
 cd build/
 cmake ..
 cmake --build .
+./my_project
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
+#include <inttypes.h>
 
-#include <etl/array.h>
-#include <etl/iarr
+#include <etl/vector.h>
+
+void passMeAVector(etl::ivector<uint32_t>& vec) {
+    // Do something with the vector
+    vec.push_back(13);
+    vec.push_back(14);
+}
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
-    etl::array<int, 6> arr;
+    etl::vector<uint32_t, 6> vec;
+    passMeAVector(vec);
+    printf("Num. of things in vec is %lu, vec[0] is %u, capacity is %lu\n", vec.size(), vec[0], vec.capacity());
     return 0;
 }
 
-void passMeAnArray(etl::a) {
 
-}
